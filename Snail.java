9f56cc2a880f3f825e539be4f06f879bac52ee49
Artyom Vecherov <a.vecherov@gmail.com>
2018-02-08 03:16:22 +0300

    All is working

--- Snail.java
+++ Snail.java
@@ -1,5 +1,7 @@
 package com.avecherov;
 
+import com.sun.corba.se.spi.presentation.rmi.IDLNameTranslator;
+
 public class Snail {
 
     public static int[] snail(int[][] array) {
@@ -7,11 +9,13 @@ public class Snail {
         int[] result = new int[array.length * array.length];
         int position = 0;
         int segment = array.length;
+        int counter = array.length - 1;
 
         if (array[0].length == 0) {
             return new int[0];
         }
-        for (int i = position; i <= segment * segment; i++) {
+
+        for (int i = position; i <= array.length * array.length; i++) {
             // Upper side
             for (int j = 0; j < segment && position < array.length * array.length; j++) {
                 result[position] = array[i][i + j];
@@ -19,23 +23,28 @@ public class Snail {
             }
 
             // Right side
-            for (int j = 1; j <= segment - 1 && position < array.length * array.length && segment > 2; j++) {
-                result[position] = array[j][segment - 1 + i];
+            for (int j = 1; j < segment - 1 && position < array.length * array.length; j++) {
+                result[position] = array[j + i][segment - 1 + i];
                 position++;
             }
             // Bottom
-            for (int j = segment - 1 + i; j > 0 && position < array.length * array.length; j--) {
-                result[position] = array[segment - 1 + i][j - 1 + i];
+            for (int j = 0; j <= segment - 1 && position < array.length * array.length; j++) {
+                result[position] = array[counter][counter - j];
                 position++;
             }
 
+
             // Left side
-            for (int j = segment-2; j > 0 && position < array.length * array.length; j--) {
-                result[position] = array[j][i];
+            for (int j = 1; j < segment-1 && position < array.length * array.length; j++) {
+                result[position] = array[counter - j][i];
                 position++;
             }
+
             segment -=2;
+            counter--;
         }
         return result;
     }
 }
+
+
--- SnailTest.java
+++ SnailTest.java
@@ -9,49 +9,28 @@ import static java.util.stream.Collectors.joining;
 
 public class SnailTest {
 
-//    @Test
-//    public void SnailTest1() {
-//        int[][] array
-//                = {{1, 2, 3},
-//                {4, 5, 6},
-//                {7, 8, 9}};
-//        int[] r = {1, 2, 3, 6, 9, 8, 7, 4, 5};
-//        test(array, r);
-//    }
-//
-//    public String int2dToString(int[][] a) {
-//        return Arrays.stream(a).map(row -> Arrays.toString(row)).collect(joining("\n"));
-//    }
-//
-//    public void test(int[][] array, int[] result) {
-//        String text = int2dToString(array) + " should be sorted to " + Arrays.toString(result);
-//        System.out.println(text);
-//        Assert.assertArrayEquals( result, Snail.snail(array));
-//    }
-//
-//    @Test
-//    public void SnailTest2() {
-//        int[][] array1
-//                = {{1, 2, 3, 4},
-//                {12, 13, 14, 5},
-//                {11, 16, 15, 6},
-//                {10, 9, 8, 7}};
-//        int[] r = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
-//        test1(array1, r);
-//    }
-//
-//    public String int2dToString1(int[][] a) {
-//        return Arrays.stream(a).map(row -> Arrays.toString(row)).collect(joining("\n"));
-//    }
-//
-//    public void test1(int[][] array1, int[] result) {
-//        String text = int2dToString1(array1) + " should be sorted to " + Arrays.toString(result);
-//        System.out.println(text);
-//        Assert.assertArrayEquals( result, Snail.snail(array1));
-//    }
+    @Test
+    public void SnailTest5() {
+        int[][] array3
+                = {{1, 2},
+                   {4, 3}};
+
+        int[] r = {1, 2, 3, 4};
+        test2(array3, r);
+    }
+
+    public String int2dToString3(int[][] a) {
+        return Arrays.stream(a).map(row -> Arrays.toString(row)).collect(joining("\n"));
+    }
+
+    public void test3(int[][] array3, int[] result) {
+        String text = int2dToString2(array3) + " should be sorted to " + Arrays.toString(result);
+        System.out.println(text);
+        Assert.assertArrayEquals( result, Snail.snail(array3));
+    }
 
     @Test
-    public void SnailTest3() {
+    public void SnailTest2() {
         int[][] array2
                 = {{},
                 {},
@@ -70,5 +49,71 @@ public class SnailTest {
         System.out.println(text);
         Assert.assertArrayEquals( result, Snail.snail(array2));
     }
+
+    @Test
+    public void SnailTest3() {
+        int[][] array
+                = {{1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}};
+        int[] r = {1, 2, 3, 6, 9, 8, 7, 4, 5};
+        test(array, r);
+    }
+
+    public String int2dToString(int[][] a) {
+        return Arrays.stream(a).map(row -> Arrays.toString(row)).collect(joining("\n"));
+    }
+
+    public void test(int[][] array, int[] result) {
+        String text = int2dToString(array) + " should be sorted to " + Arrays.toString(result);
+        System.out.println(text);
+        Assert.assertArrayEquals( result, Snail.snail(array));
+    }
+
+    @Test
+    public void SnailTest4() {
+        int[][] array1
+                = {{1, 2, 3, 4},
+                {12, 13, 14, 5},
+                {11, 16, 15, 6},
+                {10, 9, 8, 7}};
+        int[] r = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+        test1(array1, r);
+    }
+
+    public String int2dToString1(int[][] a) {
+        return Arrays.stream(a).map(row -> Arrays.toString(row)).collect(joining("\n"));
+    }
+
+    public void test1(int[][] array1, int[] result) {
+        String text = int2dToString1(array1) + " should be sorted to " + Arrays.toString(result);
+        System.out.println(text);
+        Assert.assertArrayEquals( result, Snail.snail(array1));
+    }
+
+    @Test
+    public void SnailTest1() {
+        int[][] array5
+                = {{1,  2,  3,  4, 5},
+                  {16, 17, 18, 19, 6},
+                  {15, 24, 25, 20, 7},
+                  {14, 23, 22, 21, 8},
+                  {13, 12, 11, 10, 9}};
+        int[] r = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,18,19,20,21,22,23,24,25};
+        test1(array5, r);
+    }
+
+    public String int2dToString5(int[][] a) {
+        return Arrays.stream(a).map(row -> Arrays.toString(row)).collect(joining("\n"));
+    }
+
+    public void test5(int[][] array5, int[] result) {
+        String text = int2dToString5(array5) + " should be sorted to " + Arrays.toString(result);
+        System.out.println(text);
+        Assert.assertArrayEquals( result, Snail.snail(array5));
+    }
+
+
+
 
 }
